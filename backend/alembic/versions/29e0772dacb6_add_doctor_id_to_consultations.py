2f33ed0ca1283f802ac94b63900710c73b088278
Ashok <kottuashok2007@gmail.com>
2026-08-22 17:06:30 +0530

    Fix doctor_id migration

--- backend/alembic/versions/29e0772dacb6_add_doctor_id_to_consultations.py
+++ backend/alembic/versions/29e0772dacb6_add_doctor_id_to_consultations.py
@@ -1,3 +1,4 @@
+
 """add doctor id to consultations
 
 Revision ID: 29e0772dacb6
@@ -5,9 +6,11 @@ Revises: 449bebcc3d92
 Create Date: 2026-08-22 11:43:57.249714
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
+import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
@@ -18,10 +21,47 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    """Doctor ID already exists in the database."""
-    pass
+    """Add doctor_id to consultations."""
+    op.add_column(
+        "consultations",
+        sa.Column(
+            "doctor_id",
+            sa.Integer(),
+            nullable=True,
+        ),
+    )
+
+    op.create_foreign_key(
+        "fk_consultations_doctor_id_users",
+        "consultations",
+        "users",
+        ["doctor_id"],
+        ["id"],
+    )
+
+    op.create_index(
+        "ix_consultations_doctor_id",
+        "consultations",
+        ["doctor_id"],
+        unique=False,
+    )
 
 
 def downgrade() -> None:
-    """Do not remove doctor_id."""
-    pass
+    """Remove doctor_id from consultations."""
+    op.drop_index(
+        "ix_consultations_doctor_id",
+        table_name="consultations",
+    )
+
+    op.drop_constraint(
+        "fk_consultations_doctor_id_users",
+        "consultations",
+        type_="foreignkey",
+    )
+
+    op.drop_column(
+        "consultations",
+        "doctor_id",
+    )
+
